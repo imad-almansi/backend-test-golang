4aa42dbdc82645cb9f54ac03511958c99795b87e
Imad AlMansi <imadalmansi@yahoo.com>
2023-07-10 01:11:09 +0200

    add propper http results

--- pkg/handlers/facts.go
+++ pkg/handlers/facts.go
@@ -3,10 +3,10 @@ package handlers
 import (
 	"context"
 	"fmt"
-	"log"
 	"net/http"
 	"strconv"
 
+	"github.com/imad-almansi/backend-test-golang/pkg/handlers/results"
 	"github.com/imad-almansi/backend-test-golang/pkg/model"
 	"github.com/imad-almansi/backend-test-golang/pkg/mongodb"
 	"go.mongodb.org/mongo-driver/bson"
@@ -26,7 +26,8 @@ func HandleFacts(rw http.ResponseWriter, req *http.Request) {
 	if foundFilter != "" {
 		foundBoolean, err := strconv.ParseBool(foundFilter)
 		if err != nil {
-			log.Fatal("found filter is an invalid boolean: ", err)
+			results.Negative(rw, fmt.Errorf("found filter is an invalid boolean: %w", err), 400)
+			return
 		}
 		filter = mongodb.FilterLiteral("found", foundBoolean, filter)
 	}
@@ -41,20 +42,24 @@ func HandleFacts(rw http.ResponseWriter, req *http.Request) {
 	if limit != "" {
 		limitInt, err := strconv.ParseInt(limit, 10, 64)
 		if err != nil {
-			log.Fatal("limit is an invalid Int64: ", err)
+			results.Negative(rw, fmt.Errorf("limit is an invalid Int64: %w", err), 400)
+			return
 		}
 		findOptions = append(findOptions, options.Find().SetLimit(limitInt))
 	}
 
 	cur, err := mongodb.Collection.Find(context.Background(), filter, findOptions...)
 	if err != nil {
-		log.Fatal("Find failed: ", err)
+		results.Negative(rw, fmt.Errorf("Find failed: %w", err), 500)
+		return
 	}
 
-	var results []model.Fact
-	err = cur.All(context.Background(), &results)
+	var result []model.Fact
+	err = cur.All(context.Background(), &result)
 	if err != nil {
-		log.Fatal("Decode failed: ", err)
+		results.Negative(rw, fmt.Errorf("Decoding results failed: %w", err), 500)
+		return
 	}
-	fmt.Println(results)
+
+	results.Positive(rw, result)
 }
---
+++ pkg/handlers/results/results.go
@@ -0,0 +1,30 @@
+package results
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/imad-almansi/backend-test-golang/pkg/model"
+)
+
+func Positive(rw http.ResponseWriter, result []model.Fact) {
+	jsonResult, err := json.MarshalIndent(result, "", "\t")
+	if err != nil {
+		Negative(rw, fmt.Errorf("failed to marshal response, error was: %s", err), 500)
+		return
+	}
+
+	_, err = rw.Write(jsonResult)
+	if err != nil {
+		Negative(rw, fmt.Errorf("failed to write response, error was: %s", err), 500)
+		return
+	}
+}
+
+func Negative(rw http.ResponseWriter, err error, statusCode int) {
+	log.Printf("error occured: %s", err)
+
+	http.Error(rw, http.StatusText(statusCode), statusCode)
+}
